2886dff71a10455b017d245db5c7d9fe64644e70
giguchi <hil.giga@gmail.com>
2023-01-10 23:31:50 +0000

    add socket server

--- src/feature/socket/index.js
+++ src/feature/socket/index.js
@@ -1,8 +1,5 @@
 import {io} from "socket.io-client"
 
-// const socket = (username) => io("http://localhost:8900/", {autoConnect: false, query: {data: username}})
-const socket = io("http://localhost:8900/", {autoConnect: false})
-
-// const socket = io("http://localhost:8900/hello", {autoConnect: false})
+const socket = io("https://chatter-server-d9jzp.ondigitalocean.app/roomer-server2", {autoConnect: false})
 
 export default  socket
